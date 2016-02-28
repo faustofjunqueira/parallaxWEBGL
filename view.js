03d7ff4f90cdfb6cb077455f73a9ac853be62d41
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-02-28 15:01:23 -0300

    remove all

--- view.js
+++
@@ -1,9 +0,0 @@
-var View = function(canvasId){
-  return {
-    canvas: document.getElementById( canvasId ),
-    shader: {
-      vertex: 'vertex-shader',
-      fragment: 'fragment-shader'
-    }
-  };
-}
